19dcd0f55b061b61bf27a68adbeb55e986ab46a9
yiyiyimu <wosoyoung@gmail.com>
2020-11-24 17:37:02 +0800

    add ns-ls && rename repo
    Signed-off-by: yiyiyimu <wosoyoung@gmail.com>

--- README.md
+++ README.md
@@ -1,9 +1,14 @@
-# nscat
-Enter certain **NS** and **CAT** file contents
+# nsexec
+Enter certain **NS** and **EXEC**ute command in mount namespace.
 
-For really slim container that even remove `cat`, since nsenter to mount namespace could not find the command file in target ns, this tool could achieve enter a namespace and cat certain file.
+For really slim container that even remove `cat`/`ls`, since nsenter to mount namespace could not find the command file in target ns,
+this tool could achieve enter a namespace and print file / list directory.
+
+### ls
+Currently support `-l`, to also print symlink when needed.
 
 ### Example
 ```shell
-$ sudo nscat /proc/$PID/ns/mnt $FILEPATH
+$ sudo nsexec $PID cat $FILEPATH
+$ sudo nsexec $PID ls -l $DIR
 ```
--- src/main.rs
+++ src/main.rs
@@ -1,42 +1,73 @@
 use nix::sched;
+use nix::unistd;
 use std::error::Error;
+use std::ffi::{CStr, CString};
 use std::fs::{self, File};
 use std::os::unix::io::IntoRawFd;
+use std::path::Path;
 use std::{env, process};
 
 fn main() -> Result<(), Box<dyn Error>> {
     let args: Vec<String> = env::args().collect();
-    if args.len() < 3 {
+    if args.len() < 4 {
         eprintln!("Not enough argurments!");
         process::exit(1);
     }
 
-    let fd = File::open(args[1].clone()).unwrap_or_else(|err| {
-        println!("Failed to open file: {}", err);
-        process::exit(1);
-    });
-
+    let ns = format!("/proc/{}/ns/mnt", args[1].clone());
+    let fd = File::open(ns)?;
     sched::setns(fd.into_raw_fd(), sched::CloneFlags::CLONE_NEWNS).unwrap_or_else(|err| {
-        println!("Failed to setns: {}", err);
+        eprintln!("Failed to setns: {}", err);
         process::exit(1);
     });
 
-    //FOR NSENTER
-    //use nix::unistd;
-    //use std::ffi::{CStr, CString};
-    //let cmd_cstring = vec![CString::new(args[2].clone()).unwrap()];
-    //let cmd: Vec<&CStr> = cmd_cstring.iter().map(CString::as_c_str).collect();
-    //unistd::execvp(&cmd[0], &cmd).expect("execvp failed");
-    cat(args[2].clone());
+    let cmd = args[2].clone();
+    match cmd.as_str() {
+        "cat" => cat(args[3].clone())?,
+        "ls" => ls(&args[3..])?,
+        "exec" => exec(args[3].clone())?,
+        _ => eprintln!("Command not supported."),
+    }
 
     Ok(())
 }
 
-fn cat(filename: String) {
-    let contents = fs::read_to_string(filename).unwrap_or_else(|err| {
-        println!("Failed to print file: {}", err);
-        process::exit(1);
-    });
-
+fn cat(filename: String) -> Result<(), Box<dyn Error>> {
+    let contents = fs::read_to_string(filename)?;
     println!("{}", contents);
+    Ok(())
+}
+
+fn ls(args: &[String]) -> Result<(), Box<dyn Error>> {
+    let is_long = args[0] == "-l";
+    let path_string = if is_long {
+        args[1].clone()
+    } else {
+        args[0].clone()
+    };
+    let path = Path::new(&path_string);
+
+    for entry in fs::read_dir(path)? {
+        let entry = entry?;
+        let name = entry.file_name().into_string().unwrap();
+        if is_long {
+            let meta = entry.metadata()?;
+            if meta.file_type().is_symlink() {
+                let symlink_file = fs::read_link(entry.path())?;
+                println!("{} -> {:?}", name, symlink_file);
+            } else {
+                println!("{}", name);
+            }
+        } else {
+            print!("{} ", name);
+        }
+    }
+    Ok(())
+}
+
+fn exec(cmd_in: String) -> Result<(), Box<dyn Error>> {
+    let cmd_cstring = vec![CString::new(cmd_in).unwrap()];
+    let cmd: Vec<&CStr> = cmd_cstring.iter().map(CString::as_c_str).collect();
+    unistd::execvp(&cmd[0], &cmd)?;
+    Ok(())
 }
